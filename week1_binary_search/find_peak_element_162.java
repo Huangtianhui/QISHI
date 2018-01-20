9fc176a787f83a56830dfc54b8705201b886a019
[Huangtianhui] <[huihuijiang0619@gmail.com]>
2018-01-19 21:16:38 -0500

    update week1

--- week1_binary_search/find_peak_element_162.java
+++ week1_binary_search/find_peak_element_162.java
@@ -1,4 +1,18 @@
-
-public class find_peak_element_162 {
-
+public class find_peak_element {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] < nums[mid-1]) {
+                end = mid;
+            } else if(nums[mid] < nums[mid+1]) {
+                start = mid;
+            } else {
+                return mid;
+            }
+        }
+        return nums[start] >= nums[end] ? start : end;
+    }
 }
+
--- week1_binary_search/heaters_475.java
+++ week1_binary_search/heaters_475.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
-public class heaters_475 {
 
+public class heaters {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int n = houses.length;
+        int m = heaters.length;
+        int minimum = 0;
+        int j = 0;
+        for(int i = 0 ;i< n ;i++){
+            while( j < m - 1 && (Math.abs(heaters[j] - houses[i]) >= Math.abs(heaters[j+1] - houses[i])))
+                j ++;
+            //System.out.println(i+" "+j);
+            minimum = Math.max(minimum,Math.abs(heaters[j] - houses[i]));
+        }
+        return minimum;
+        
+    }
 }
